74f021c65d12375a1270e9fef0b72546050b2778
AYMAN <126827455+AymanMady@users.noreply.github.com>
2025-02-03 15:37:29 +0000

    add pages front

--- teacher-place/src/app/app.component.ts
+++ teacher-place/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthComponent } from './modules/auth/auth.component';
+import { HomeComponent } from './modules/home/home.component';
+import { AttendancesComponent } from './modules/attendances/attendances.component';
+import { AttendanceGridComponent } from './modules/attendance-grid/attendance-grid.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AuthComponent,HomeComponent,AttendancesComponent,AttendanceGridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ teacher-place/src/app/modules/attendances/attendances.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+
+@Component({
+  selector: 'app-attendances',
+  imports: [HeaderComponent],
+  templateUrl: './attendances.component.html',
+  styleUrl: './attendances.component.css'
+})
+export class AttendancesComponent {
+
+}
